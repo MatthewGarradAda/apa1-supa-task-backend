82ab568562bb0969334fd1485ec416fa24bb30ed
MatthewGarradAda <matthew.garrad@booking.com>
2025-02-25 16:23:14 +0000

    Project init

---
+++ migrations/20250220142429_remote_schema.sql
@@ -0,0 +1,63 @@
+create sequence "public"."users_id_seq";
+
+create table "public"."users" (
+    "id" integer not null default nextval('users_id_seq'::regclass),
+    "name" text not null,
+    "email" text not null,
+    "created_at" timestamp without time zone default now()
+);
+
+
+alter sequence "public"."users_id_seq" owned by "public"."users"."id";
+
+CREATE UNIQUE INDEX users_email_unique ON public.users USING btree (email);
+
+CREATE UNIQUE INDEX users_pkey ON public.users USING btree (id);
+
+alter table "public"."users" add constraint "users_pkey" PRIMARY KEY using index "users_pkey";
+
+alter table "public"."users" add constraint "users_email_unique" UNIQUE using index "users_email_unique";
+
+grant delete on table "public"."users" to "anon";
+
+grant insert on table "public"."users" to "anon";
+
+grant references on table "public"."users" to "anon";
+
+grant select on table "public"."users" to "anon";
+
+grant trigger on table "public"."users" to "anon";
+
+grant truncate on table "public"."users" to "anon";
+
+grant update on table "public"."users" to "anon";
+
+grant delete on table "public"."users" to "authenticated";
+
+grant insert on table "public"."users" to "authenticated";
+
+grant references on table "public"."users" to "authenticated";
+
+grant select on table "public"."users" to "authenticated";
+
+grant trigger on table "public"."users" to "authenticated";
+
+grant truncate on table "public"."users" to "authenticated";
+
+grant update on table "public"."users" to "authenticated";
+
+grant delete on table "public"."users" to "service_role";
+
+grant insert on table "public"."users" to "service_role";
+
+grant references on table "public"."users" to "service_role";
+
+grant select on table "public"."users" to "service_role";
+
+grant trigger on table "public"."users" to "service_role";
+
+grant truncate on table "public"."users" to "service_role";
+
+grant update on table "public"."users" to "service_role";
+
+
